265220208b38c74dfb7f673fbf53a8fe4e230ef1
gkluz <grkluz@gmail.com>
2023-10-02 12:06:15 +0100

    list test

--- src/test/java/com/example/testinunit/ListMockTest.java
+++ src/test/java/com/example/testinunit/ListMockTest.java
@@ -1,5 +1,32 @@
 package com.example.testinunit;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 public class ListMockTest {
-//    test
+    List mock =mock(List.class);
+    @Test
+    public void test(){
+        when(mock.size()).thenReturn(5);
+        assertEquals(5,mock.size());
+    }
+
+    @Test
+    public void returnDifferentValues(){
+        when(mock.size()).thenReturn(10).thenReturn(5);
+        assertEquals(5,mock.size());
+        assertEquals(10,mock.size());
+    }
+
+    @Test
+    public void returnWithParameters(){
+        when(mock.get(0)).thenReturn("test");
+        when(mock.get(2)).thenReturn("test2");
+        assertEquals("test",mock.get(0));
+        assertEquals("test2",mock.get(1));
+    }
 }
